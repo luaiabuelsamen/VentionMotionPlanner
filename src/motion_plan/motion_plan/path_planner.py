6034cdd6da72257359cd569cd343847272d29435
Jetson <luai.abuelsamen@mail.mcgill.ca>
2024-12-12 21:27:26 -0800

    dynamic collision avoidance

--- src/motion_plan/motion_plan/path_planner.py
+++ src/motion_plan/motion_plan/path_planner.py
@@ -1,11 +1,11 @@
 #PYTHON IMPORTS
 import os
 import yaml
+import time
 
 #ROS2 IMPORTS
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import JointState
 from ament_index_python.packages import get_package_share_directory
 
 #CUROBO IMPORTS
@@ -16,12 +16,13 @@ from curobo.wrap.reacher.motion_gen import MotionGen, MotionGenConfig, MotionGen
 from curobo.types.base import TensorDeviceType
 from curobo.wrap.model.robot_world import RobotWorld, RobotWorldConfig
 
+#LOCAL IMPORTS
+from motion_plan.rviz_publisher import RvizPublisher
 
-class JointStatePublisher(Node):
+class CuroboMotionPlanner(Node):
     def __init__(self):
-        super().__init__('joint_state_publisher')
-        self.publisher = self.create_publisher(JointState, '/joint_states', 10)
-        self.joint_names = [
+        super().__init__('curobo_motion_planner')
+        joint_names = [
             "shoulder_pan_joint",
             "shoulder_lift_joint",
             "elbow_joint",
@@ -29,56 +30,72 @@ class JointStatePublisher(Node):
             "wrist_2_joint",
             "wrist_3_joint",
         ]
-        self.joint_positions = self.generate_joint_positions()
-        self.timer = self.create_timer(0.1, self.publish_joint_states)
-        self.current_step = 0
+        self.robot_config_file = "ur5e.yml"
+        self.get_logger().info("Motion Planner Initialized")
+        self.init_world_config()
 
-    def generate_joint_positions(self):
-        robot_config_file = "ur5e.yml"
+        self.rviz_publisher = RvizPublisher(joint_names=joint_names, obstacles = self.obstacles)
+        self.create_timer(0.5, self.rviz_publisher.publish_obstacles)
+        self.create_timer(0.5, self.rviz_publisher._persist_joint_state)
+
         current_position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        goal_position = [-0.4, 0.0, 0.4, 1.0, 0.0, 0.0, 0.0]
+        goal_position = [0.4, -0.4, 0.4, 1.0, 0.0, 0.0, 0.0]
+        self.joint_positions = self.generate_joint_positions(current_position, goal_position)
 
-        motion_gen_config = MotionGenConfig.load_from_robot_config(
-            robot_config_file,
-            interpolation_dt=0.01,
-        )
-        motion_gen = MotionGen(motion_gen_config)
-        motion_gen.warmup()
-
-        start_state = RoboJointState.from_position(torch.tensor([current_position], device="cuda:0"))
-        goal_pose = Pose.from_list(goal_position)
-
-        result = motion_gen.plan_single(
-            start_state, goal_pose, MotionGenPlanConfig(max_attempts=1)
-        )
-
-        if result.success.item():
-            joint_positions = [state.position.tolist() for state in result.get_interpolated_plan()]
-            self.get_logger().info("Trajectory Generated Successfully!")
-            return joint_positions
-        else:
-            self.get_logger().error("Trajectory Generation Failed.")
-            return []
-
-    def collision_checker(self, joint_states):
-        # Load YAML file
+    def init_world_config(self):
+        self.get_logger().info("Loading World")
         package_share_directory = get_package_share_directory("motion_plan")
         yaml_file = os.path.join(package_share_directory, "configs", "world", "collision_test.yml")
 
         self.get_logger().info(f"Loading obstacles from {yaml_file}")
         with open(yaml_file, "r") as f:
             self.obstacles = yaml.safe_load(f)
-
-        robot_file = "ur5e.yml"
         self.tensor_args = TensorDeviceType()
         self.world_config = RobotWorldConfig.load_from_config(
-            robot_file,
+            self.robot_config_file,
             yaml_file,
             collision_activation_distance=0.0
         )
         self.curobo_fn = RobotWorld(self.world_config)
 
-        # Perform collision checks
+        motion_gen_config = MotionGenConfig.load_from_robot_config(
+            self.robot_config_file,
+            interpolation_dt=0.01,
+            world_model=self.obstacles
+        )
+        self.motion_gen = MotionGen(motion_gen_config)
+        self.motion_gen.warmup()
+
+    def generate_joint_positions(self, current_position, goal_position):
+        start_state = RoboJointState.from_position(torch.tensor([current_position], device="cuda:0"))
+        goal_pose = Pose.from_list(goal_position)
+
+        result = self.motion_gen.plan_single(
+            start_state, goal_pose, MotionGenPlanConfig(max_attempts=1)
+        )
+
+        if result.success.item():
+            '''
+            resulting_plan contains the following info in steps
+            joint angles
+            acceleration
+            velocity
+            jerk
+            joint names
+            '''
+            resulting_plan = result.get_interpolated_plan()
+            joint_positions = resulting_plan.position.tolist()
+            self.get_logger().info("Trajectory Generated Successfully!")
+            for joint_position in joint_positions:
+                self.rviz_publisher.publish_joint_states(joint_position)
+                time.sleep(0.01)
+                self.get_logger().debug('Published')
+            return joint_positions
+        else:
+            self.get_logger().error("Trajectory Generation Failed.")
+            return []
+
+    def collision_checker(self, joint_states):
         q_sph = torch.tensor([joint_states], device=self.tensor_args.device, dtype=self.tensor_args.dtype)
         d_world, d_self = self.curobo_fn.get_world_self_collision_distance_from_joints(q_sph)
 
@@ -87,10 +104,9 @@ class JointStatePublisher(Node):
         else:
             self.get_logger().info(f"No collision. World: {d_world.item():.3f}, Self: {d_self.item():.3f}")
 
-
 def main(args=None):
     rclpy.init(args=args)
-    node = JointStatePublisher()
+    node = CuroboMotionPlanner()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
--- src/motion_plan/motion_plan/rviz_publisher.py
+++ src/motion_plan/motion_plan/rviz_publisher.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
-
-import os
-import yaml
-
-from ament_index_python.packages import get_package_share_directory
-
+import threading
 
 from sensor_msgs.msg import JointState
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
+from rclpy.node import Node
 
-class RvizPublisher():
-    def __init__(self):
+class RvizPublisher(Node):
+    def __init__(self, joint_names, obstacles):
+        super().__init__('rviz_publisher')
 
         # Publishers
         self.joint_publisher = self.create_publisher(JointState, "/joint_states", 10)
         self.marker_publisher = self.create_publisher(MarkerArray, "/obstacle_markers", 10)
 
         # Robot joint names
-        self.joint_names = [
-            "shoulder_pan_joint",
-            "shoulder_lift_joint",
-            "elbow_joint",
-            "wrist_1_joint",
-            "wrist_2_joint",
-            "wrist_3_joint",
-        ]
+        self.joint_names = joint_names
+        self.obstacles = obstacles
 
-        self.create_timer(0.1, self.publish_joint_states)  # 10 Hz
-        self.create_timer(0.5, self.publish_obstacles)  # 2 Hz
-
-        self.get_logger().info("Collision Avoidance Joint Publisher Initialized")
+        self.get_logger().info("RVIZ Publisher Initialized")
 
     def publish_joint_states(self, joint_angles):
         joint_state = JointState()
@@ -38,36 +26,42 @@ class RvizPublisher():
         joint_state.name = self.joint_names
 
         joint_state.position = joint_angles
+        self.current_joint_state = joint_state
         self.joint_publisher.publish(joint_state)
 
+    def _persist_joint_state(self):
+        """Continuously publish the last known state."""
+        rate = self.create_rate(10)
+        while True:
+            if self.current_joint_state:
+                self.joint_publisher.publish(self.current_joint_state)
+            rate.sleep()
+    
     def publish_obstacles(self):
-        pass
-        # marker_array = MarkerArray()
-        # for i, (obstacle_name, obstacle) in enumerate(self.obstacles["cuboid"].items()):
-        #     marker = Marker()
-        #     marker.header.frame_id = "world"
-        #     marker.header.stamp = self.get_clock().now().to_msg()
-        #     marker.ns = "obstacles"
-        #     marker.id = i
-        #     marker.type = Marker.CUBE
-        #     marker.action = Marker.ADD
-        #     marker.scale.x = obstacle["dims"][0]
-        #     marker.scale.y = obstacle["dims"][1]
-        #     marker.scale.z = obstacle["dims"][2]
-        #     marker.color.a = 1.0  # Fully opaque
-        #     marker.color.r = 1.0  # Red for visibility
-        #     marker.color.g = 0.0
-        #     marker.color.b = 0.0
-        #     marker.pose.position.x = obstacle["pose"][0]
-        #     marker.pose.position.y = obstacle["pose"][1]
-        #     marker.pose.position.z = obstacle["pose"][2]
-        #     marker.pose.orientation.x = float(obstacle["pose"][3])
-        #     marker.pose.orientation.y = float(obstacle["pose"][4])
-        #     marker.pose.orientation.z = float(obstacle["pose"][5])
-        #     marker.pose.orientation.w = float(obstacle["pose"][6])
+        marker_array = MarkerArray()
+        for i, (obstacle_name, obstacle) in enumerate(self.obstacles["cuboid"].items()):
+            marker = Marker()
+            marker.header.frame_id = "world"
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.ns = "obstacles"
+            marker.id = i
+            marker.type = Marker.CUBE
+            marker.action = Marker.ADD
+            marker.scale.x = obstacle["dims"][0]
+            marker.scale.y = obstacle["dims"][1]
+            marker.scale.z = obstacle["dims"][2]
+            marker.color.a = 1.0  # Fully opaque
+            marker.color.r = 0.0  # Red for visibility
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.pose.position.x = obstacle["pose"][0]
+            marker.pose.position.y = obstacle["pose"][1]
+            marker.pose.position.z = obstacle["pose"][2]
+            marker.pose.orientation.x = float(obstacle["pose"][3])
+            marker.pose.orientation.y = float(obstacle["pose"][4])
+            marker.pose.orientation.z = float(obstacle["pose"][5])
+            marker.pose.orientation.w = float(obstacle["pose"][6])
 
-        #     marker_array.markers.append(marker)
+            marker_array.markers.append(marker)
 
-        # self.marker_publisher.publish(marker_array)
-        # self.get_logger().info(f"Published {len(marker_array.markers)} obstacle markers.")
-
+        self.marker_publisher.publish(marker_array)
